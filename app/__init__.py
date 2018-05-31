71dda93a52bc13e487bba5ee7966174f325f3a9b
mdmhdc <451827595@qq.com>
2018-05-31 14:15:17 +0800

    delete some comments

--- app/__init__.py
+++ app/__init__.py
@@ -2,25 +2,11 @@
 # coding=utf-8
 
 from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from flask_mail import Mail
-# from flask_sqlalchemy import SQLAlchemy
-# from config import config
-
-# bootstrap = Bootstrap()
-# mail = Mail()
-# db = SQLAlchemy()
 
 from .main import main
 
 def create_app(config_name=''):
   app = Flask(__name__)
-  # app.config.from_object(config[config_name])
-  # config[config_name].init_app(app)
-
-  # bootstrap.init_app(app)
-  # mail.init_app(app)
-  # db.init_app(app)
 
   app.register_blueprint(main)
 
